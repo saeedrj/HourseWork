a5eb26e31810805152bb2c38ecc45fbcbe10dcdd
saeedrj <saeedrajaee446@gmail.com>
2023-09-27 17:55:36 +0330

    first cm

---
+++ src/main/java/com/org/hourswork/HoursWorkApplication.java
@@ -0,0 +1,13 @@
+package com.org.hourswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoursWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoursWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/hourswork/HoursWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.hourswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoursWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
